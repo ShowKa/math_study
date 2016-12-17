38786ee76cef15fd88bfb87a2c44d372006e9037
ShowKa <kanamoto.official@gmail.com>
2016-12-17 15:31:17 +0900

    Inverse and Similarity

--- housefolder.js
+++ housefolder.js
@@ -13,16 +13,34 @@ let a = new Matrix([
     [-2, 2, 3, 4],
     [2, 2, 3, 4]
 ]);
-
 a.housefolder();
 
+// linear equation
 let b = new Matrix([
     [1, 1, 1],
     [3, 2, -2],
     [2, -1, 3]
 ]);
+l(b.linearEquation(new Vector(6, 1, 9)));
 
-let v = new Vector(6, 1, 9);
 
-let answer = b.linearEquation(v);
-l(answer);
+// get inverse
+let c = new Matrix([
+    [1, 1, -1],
+    [-2, 0, 1],
+    [0, 2, 1]
+]);
+l(c.getInverse());
+
+// transform similarity
+let d = new Matrix([
+    [5, 4],
+    [1, 2]
+]);
+
+let p = new Matrix([
+    [4, 1],
+    [1, -1]
+]);
+
+l(d.transformSimilarity(p));
